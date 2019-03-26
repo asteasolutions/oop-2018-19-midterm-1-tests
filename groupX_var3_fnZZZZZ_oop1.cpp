f189f407d9992c9c85c16d749e1ea82bd99ae106
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2019-03-27 00:07:14 +0200

    Примерно решение на трета задача

--- groupX_var3_fnZZZZZ_oop1.cpp
+++ groupX_var3_fnZZZZZ_oop1.cpp
@@ -4,6 +4,11 @@
 // FN: попълнете факултетния си номер тук
 // Date: 27.03.2019
 //
+#include <cstring>
+#include <iostream>
+
+const int MAX_STRING = 100;
+const int MAX_DATE = 3 * 2 + 2 + 1;
 
 ///
 /// Да се реализира клас Measurement, съхраняващ информация за едно измерване на температурата със следните атрибути:
@@ -13,24 +18,33 @@
 ///
 class Measurement {
   // моля, попълнете реализацията на класа тук
+  char location[MAX_STRING];
+  char date[MAX_DATE];
+  double temperature;
 
 public:
   // За класа да се реализират:
   // подходящи конструктори, например:
 
   // конструктор с параметри
-  Measurement(char const* location, char const* time, int length);
+  Measurement(char const* location = "<unknown location>", char const* date="01/01/80", double temperature = 0) {
+    strncpy(this->location, location, MAX_STRING - 1);
+    this->location[MAX_STRING - 1] = '\0';
+    strncpy(this->date, date, MAX_DATE - 1);
+    this->date[MAX_DATE - 1] = '\0';
+    this->temperature = temperature;
+  }
 
   // Ако е необходимо: деструктор, конструктор за копиране, оператор за присвояване
 
   // Методи за достъп до атрибутите
-  char const* getLocation() const;
-  char const* getDate() const;
-  double getTemperature() const;
+  char const* getLocation() const { return location; }
+  char const* getDate()     const { return date; }
+  double getTemperature()   const { return temperature; }
 
   // Оператор за сравнение на измервания: <= (измерванията се сравняват спрямо стойностите,
   // като другите атрибути се игнорират)
-  bool operator<=(Measurement const& other);
+  bool operator<=(Measurement const& other) const { return temperature <= other.temperature; }
 };
 
 ///
@@ -40,24 +54,93 @@ public:
 
 class MaxTemperatures {
   // моля, попълнете реализацията на класа тук
+private:
+  Measurement* items;
+  unsigned capacity;
+  unsigned size;
+
+  void copy(MaxTemperatures const& other) {
+    capacity = other.capacity;
+    size = other.size;
+    items = new Measurement[other.capacity];
+    for (unsigned i = 0; i < other.size; i++) {
+      items[i] = other.items[i];
+    }
+  }
 
 public:
   // За класа да се реализират:
   // Конструктор с един параметър, задаващ максималния брой N на измервания, които ще бъдат съхранявани в списъка
-  MaxTemperatures(unsigned N);
+  MaxTemperatures(unsigned N) : capacity(N), size(0) {
+    items = new Measurement[capacity];
+  }
   
   // Ако е необходимо: деструктор, конструктор за копиране, оператор за присвояване
+  MaxTemperatures(MaxTemperatures const& other) {
+    copy(other);
+  }
+
+  MaxTemperatures& operator=(MaxTemperatures const& other) {
+    if (this != &other) {
+        delete [] items;
+        copy(other);
+    }
+    return *this;
+  }
+
+  ~MaxTemperatures() {
+    delete [] items;
+  }
+
 
   // Метод Метод addMeasurement за добавяне на измерване към списъка (ако вече има измерване на същото място,
   // да остане само измерването с по-голяма стойност)
-  void addMeasurement(Measurement const& item);
+  void addMeasurement(Measurement const& item) {
+    // search for existing item with the same location
+    for (unsigned i = 0; i < size; i++) {
+      if (strcmp(items[i].getLocation(), item.getLocation()) == 0) {
+        // found it, take item with higher measurement
+        if (!(item <= items[i]))
+          items[i] = item;
+        return;
+      }
+    }
+    // no existing item found, check if we have capacity to add the new one
+    if (size < capacity) {
+        items[size++] = item;
+    } else {
+        std::cerr << "List is full, cannot add more items" << std::endl;
+    }
+  }
 
   // Метод findAverage, който изчислява и връща средно аритметичното на всички запазени измервания
-  double findAverage() const;
+  double findAverage() const {
+    if (size == 0) {
+      std::cerr << "Cannot find average of an empty list!" << std::endl;
+      return 0;
+    }
+    double total = 0;
+    for (unsigned i = 0; i < size; i++) {
+      total += items[i].getTemperature();
+    }
+    return total / size;
+  }
   
   // Метод highestMeasurement, който връща най-високото измерване (ако има две или повече, да се върне някое
   // от тях, без значение кое)
-  Measurement highestMeasurement() const;
+  Measurement highestMeasurement() const {
+    unsigned highestMesaurementIndex = 0;
+    if (size == 0) {
+        std::cerr << "List is empty, there is no highest measurement" << std::endl;
+        return Measurement();
+    }
+    for (unsigned i = 1; i < size; i++) {
+        if (items[i].getTemperature() > items[highestMesaurementIndex].getTemperature()) {
+            highestMesaurementIndex = i;
+        }
+    }
+    return items[highestMesaurementIndex];
+  }
 };
 
 ///
@@ -71,7 +154,7 @@ public:
 //        за да активирате автоматичните тестове за решението си.
 //
 //-------------------------------------------------------------------------
-// #include "var3-tests.h"
+#include "var3-tests.h"
 
 int main() {
 
